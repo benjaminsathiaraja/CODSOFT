14562fc65a9d0f14c69ad03a5ff6e4eab0f38a4e
BENJAMIN SATHIA RAJA S <benjamin.solomonraja@gmail.com>
2026-05-22 00:36:37 +0530

    Update Benjamin_Task2.cpp

--- Benjamin_Task2.cpp
+++ Benjamin_Task2.cpp
@@ -1,1 +1,30 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    double num1,num2;
+    char choice;
+    cout<<"Enter two numbers: ";
+    cin>>num1>>num2;
+    cout<< "Choose an operation to be performed (+, -, *, /): ";
+    cin>>choice;
+    switch(choice) {
+        case '+':
+            cout<<"Sum = "<<num1+num2<<endl;
+            break;
+        case '-':
+            cout<<"Difference= "<<num1-num2<<endl;
+            break;
+        case '*':
+            cout<<"Product = "<<num1*num2<<endl;
+            break;
+        case '/':
+            if(num2!=0)
+                cout<<"Division= "<<num1/num2<<endl;
+            else
+                cout<<"Division is not possible. Division by zero"<<endl;
+            break;
+        default:
+            cout << "Invalid operation selected" << endl;
+    }
+    return 0;
+}
